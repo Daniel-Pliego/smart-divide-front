2be5a5cb93927aa10b09a61ff1438b5c3de57165
cristianpan <panzaldivarcristian@gmail.com>
2025-11-21 09:39:50 -0600

    fix: :bug: add user balance in group list screen

--- src/features/groups/list/ui/components/UserBalance.tsx
+++ src/features/groups/list/ui/components/UserBalance.tsx
@@ -1,3 +1,4 @@
+import { Box } from "@/lib/gluestack-ui/ui/box";
 import { VStack } from "@/lib/gluestack-ui/ui/vstack";
 import { toMoney } from "@/shared/utils";
 import { HStack } from "@gluestack/hstack";
@@ -14,11 +15,17 @@ export const UserBalance = ({ totalCredits, totalDebts }: UserBalanceProps) => {
         <HStack className="mt-7 bg-white rounded-xl p-5 items-center justify-between">
             {totalBalance > 0 && (
                 <>
+                    <Box>
+                        <Text className="font-medium text-slate-900">Tu balance general</Text>
+                        <Text className="text-4xl text-purple-700">
+                            {toMoney(totalBalance)}
+                        </Text>
+                    </Box>
                     <VStack>
-                        <Text className="text-green-700 font-semibold">
+                        <Text className="text-green-700 font-semibold text-right">
                             +{toMoney(totalCredits)}
                         </Text>
-                        <Text className="text-orange-700 font-semibold">
+                        <Text className="text-orange-700 font-semibold text-right">
                             -{toMoney(totalDebts)}
                         </Text>
                     </VStack>
